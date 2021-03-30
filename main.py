226344c1193511039fd0e541145893499a3c47a4
Eduardo Arndt <eduardoarndt1@gmail.com>
2021-03-30 16:32:29 -0300

    change default population size to 100

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from cities import cities
 from functions import generate_initial_population
 
-population_size = 200
+population_size = 100
 
 population = generate_initial_population(cities, population_size)
