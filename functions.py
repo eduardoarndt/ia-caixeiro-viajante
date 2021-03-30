15ffdd708ac4e6d4947e845426c3d508382d9729
Eduardo Arndt <eduardoarndt1@gmail.com>
2021-03-30 16:11:05 -0300

    generate initial city for population generation

--- functions.py
+++ functions.py
@@ -2,9 +2,20 @@ import random
 
 def generate_initial_population(cities_list):
     population = []
+    model = {
+        'cities_list': [],
+        'initial_city': None,
+        'final_city': None
+    }
 
     for x in range(200):
+        cities_list_copy = list(cities_list)
+        model_copy = dict(model)
+
         random.shuffle(cities_list)
-        population.append(cities_list)
+        model_copy['initial_city'] = cities_list_copy.pop(0)
+        model_copy['final_city'] = model_copy['initial_city']
+        model_copy['cities_list'] = cities_list_copy
+        population.append(model_copy)
 
     return population
