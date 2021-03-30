6f0146d3c57e227368b40943d13540fd5a8b0408
Eduardo Arndt <eduardoarndt1@gmail.com>
2021-03-30 16:33:18 -0300

    add todo comment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cities import cities
 from functions import generate_initial_population
 
+# input parameters, convert them to script arguments maybe?
 population_size = 100
 
 population = generate_initial_population(cities, population_size)
