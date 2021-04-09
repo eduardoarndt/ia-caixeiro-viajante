e8295586f8639c766ea79932625c5af3d7e8b546
Eduardo Arndt <eduardoarndt1@gmail.com>
2021-04-08 21:52:37 -0300

    crossover

--- functions.py
+++ functions.py
@@ -52,28 +52,68 @@ def fitness(population):
 
 
 def select_best_half(population):
-    return population[: (len(population) - 1) // 2]
+    return population[: len(population) // 2]
 
-def crossover(a, b):
+
+def crossover_ox_simple(cities_list_a, cities_list_b):
+    a = list(cities_list_a)
+    b = list(cities_list_b)
+
     child = [None for x in range(25)]
+
+    # passo inicial - copiar meio da lista A para meio do filho
     for x in range(8, 16):
         child[x] = a[x]
 
-    a1 = a[0:8]
-    a2 = a[8:16]
-    a3 = a[16:25]
+    # remover elementos já existentes no filho da lista B
+    for x in child:
+        if x in b:
+            b.remove(x)
+
+    # copiar inicio da lista B para inicio do filho
+    child[0:8] = b[0:8]
+
+    # copiar final da lista B para final do filho
+    child[16:25] = b[8:17]
+
+    return child
+
+
+def crossover(population_a, population_b):
+    attributes_copy = dict(attributes)
+
+    population_a_copy = dict(population_a)
+    population_b_copy = dict(population_b)
+
+    a = list(population_a['cities_list'])
+    b = list(population_b['cities_list'])
+
+    attributes_copy['initial_city'] = population_a_copy['initial_city']
+    attributes_copy['final_city'] = population_a_copy['final_city']
+
+    child = [None for x in range(25)]
+    for x in range(8, 16):
+        child[x] = a[x]
 
     b1 = b[0:8]
     b2 = b[8:16]
     b3 = b[16:25]
 
-    part = b2 + b3 + b1
+    part = b3 + b1 + b2
+
+    if attributes_copy['initial_city'] in part:
+        part.remove(attributes_copy['initial_city'])
+
+    if population_b_copy['initial_city'] not in part:
+        part.append(population_b_copy['initial_city'])
 
     for x in child:
         if x in part:
             part.remove(x)
 
     child[16:25] = part[0:9]
-    child[0:8] = part[9:18]
+    child[0:8] = part[9:19]
 
-    child
+    attributes_copy['cities_list'] = child
+
+    return attributes_copy
--- main.py
+++ main.py
@@ -18,10 +18,21 @@ except Exception as exception:
 
 population = generate_initial_population(cities, population_size)
 
-# for x in range(population_size):
-#     population[x]['total_distance'] = calculate_distance(population[x]['initial_city'], population[x]['final_city'], population[x]['cities_list'])
-#
-# population = fitness(population)
-# population = select_best_half(population)
+for x in range(len(population)):
+    population[x]['total_distance'] = calculate_distance(population[x]['initial_city'], population[x]['final_city'], population[x]['cities_list'])
 
-child1 = crossover(population[0]['cities_list'], population[1]['cities_list'])
+population = fitness(population)
+population = select_best_half(population)
+
+
+i = 0
+new_population = []
+while i < len(population):
+    new_population.append(crossover(population[i], population[i + 1]))
+    new_population.append(crossover(population[i + 1], population[i]))
+    i += 2
+
+for x in range(len(new_population)):
+    new_population[x]['total_distance'] = calculate_distance(new_population[x]['initial_city'], new_population[x]['final_city'], new_population[x]['cities_list'])
+
+population = population + new_population
